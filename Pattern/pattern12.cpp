c04bdbba5a38ccf1eba5a85ab9de72a31f65d651
Asheesh Kumar <asheesh220720@gmail.com>
2023-07-19 12:47:52 +0530

    pattern12.cpp

--- Pattern/pattern12.cpp
+++ Pattern/pattern12.cpp
@@ -1,3 +1,5 @@
+//1st Approach: Naive
+
 #include<bits/stdc++.h>
 #define ll long long 
 using namespace std;
@@ -21,6 +23,34 @@ int32_t main(){
 					cout<<" ";
 					l--;
 				}
+			}
+		}
+		if(i<4) cout<<endl;
+	}
+}
+
+// 2nd Approach : Easy to Understand 
+
+#include<bits/stdc++.h>
+#define ll long long 
+using namespace std;
+int32_t main(){
+	int k,l;
+	for(int i=1;i<=4;i++){k=1,l=4;
+		for(int j=1;j<=8;j++){
+			if(j<=i || j>=9-i){
+				if(j<=4) {
+					cout<<k;
+					k++;
+				}
+				if(j>=5){
+					cout<<l;
+					l--;
+				}
+			}
+			else{
+				cout<<" ";
+				if(j>=5) l--;
 			}
 		}
 		if(i<4) cout<<endl;
