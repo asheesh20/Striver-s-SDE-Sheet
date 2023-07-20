cbc99bc04571a39436788d011050dcd9222cb3c5
Asheesh Kumar <asheesh220720@gmail.com>
2023-07-20 20:51:03 +0530

    Add files via upload

---
+++ Basics/t_pattern1.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+int32_t main(){
+	for(int i=1;i<=5;i++){
+		for(int j=1;j<=5;j++){
+			if(j<=5) cout<<"*";
+			else cout<<" ";
+		}
+		if(i<5) cout<<endl;
+	}
+}
