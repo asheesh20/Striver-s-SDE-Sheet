bc96e2d27b8a3c734e5b416114990b3a6c6ce41c
Asheesh Kumar <asheesh220720@gmail.com>
2023-07-22 21:37:43 +0530

    Pattern 7

---
+++ Pattern/t_pattern7.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#define ll long long 
+using namespace std;
+int32_t main(){
+	for(int i=1;i<=5;i++){
+		for(int j=1;j<=9;j++){
+			if(j>=6-i && j<=4+i) cout<<"*";
+			else cout<<" ";
+		}
+		if(i<5) cout<<endl;
+	}
+}
